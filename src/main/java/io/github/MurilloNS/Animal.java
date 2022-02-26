da61baf135b65ab587d095b4402c67d01837b94a
MurilloNS <murillo.murillo2001@gmail.com>
2022-02-25 23:41:39 -0300

    First Commit

---
+++ src/main/java/io/github/MurilloNS/Animal.java
@@ -0,0 +1,5 @@
+package io.github.MurilloNS;
+
+public interface Animal {
+    void fazerBarulho();
+}
---
+++ src/main/java/io/github/MurilloNS/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.MurilloNS.repository;
+
+import io.github.MurilloNS.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base e salvar o cliente
+    }
+}
